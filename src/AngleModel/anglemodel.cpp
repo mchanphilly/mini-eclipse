42ca70a51380f11c29025ee1a94b14a52b62491d
mchanphilly <56936452+mchanphilly@users.noreply.github.com>
2022-12-25 18:00:07 -0500

    Set-up angle data types

--- src/AngleModel/anglemodel.cpp
+++ src/AngleModel/anglemodel.cpp
@@ -1,33 +1,71 @@
-class GlobalCoordinates {
-    float latitude;
-    float longitude;
+#include <iostream>
+// export module AngleModel;
+
+
+class Angle {
+    // Generally stored in degrees between some minimum and max angle.
+    // By convention, we keep it between 0 and 360 degrees.
+    public:
+        Angle(double v)
+            : value(v)
+        {}
+            
+    private:
+        double value;
+};
+
+
+class AngularCoordinates {
+    public:
+        AngularCoordinates(double lat, double lon)
+            : latitude(Angle(lat)),
+              longitude(Angle(lon))
+        {}
+
+        AngularCoordinates(Angle lat, Angle lon)
+            : latitude(lat),
+              longitude(lon)
+        {}
+
+    private:
+        Angle latitude;
+        Angle longitude;
 };
 
 class SunAngles {
-    float azimuth;
-    float altitude;
+    // Coordinates for the original installation site
+    AngularCoordinates home =
+        AngularCoordinates(42.360015, -71.087902);
 
-    public:
+    // public:
     
-    const SunAngles adjust(const SunAngles offset) {
-        const float outAzimuth = azimuth + offset.azimuth;
-        const float outAltitude = altitude + offset.altitude;
-    };
+
+    // // const SunAngles adjust(const SunAngles offset) {
+    // //     const float outAzimuth = azimuth + offset.azimuth;
+    // //     const float outAltitude = altitude + offset.altitude;
+    // // };
+
+    // private:
+    //     AngularCoordinates location;
 };
 
 
 class AngleModel {
-    const GlobalCoordinates location;
-    const SunAngles windowOffset;
+        // AngularCoordinates location;
+        // SunAngles windowOffset;
 
-    SunAngles calculateSolarCoordinates(float time) {
-        // Using the approximate algorithms given on https://gml.noaa.gov/grad/solcalc/calcdetails.html
-        // TODO
-    };
+    // SunAngles calculateSolarCoordinates(float time) {
+    //     // Using the approximate algorithms given on https://gml.noaa.gov/grad/solcalc/calcdetails.html
+    //     // TODO
+    // };
 
-    SunAngles calculateWindowCoordinates(float time) {
-        SunAngles northAngles = calculateSolarCoordinates(time);
+    // SunAngles calculateWindowCoordinates(float time) {
+    //     SunAngles northAngles = calculateSolarCoordinates(time);
 
-        const SunAngles windowAngles = northAngles.adjust(windowOffset);
-    }
+    //     const SunAngles windowAngles = northAngles.adjust(windowOffset);
+    // };
+};
+
+int main() {
+    std::cout << "Test\n";
 };
