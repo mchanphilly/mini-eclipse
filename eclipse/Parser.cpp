70652674f6a82805cb155d64c5748225742f0118
mchanphilly <56936452+mchanphilly@users.noreply.github.com>
2023-02-05 19:33:39 -0500

    Split parser into H and CPP

---
+++ eclipse/Parser.cpp
@@ -0,0 +1,72 @@
+#include "Parser.h"
+
+const Parser::Command Parser::empty = Parser::Command(Parser::CommandType::Invalid, 0, 0);
+
+Parser::Command::Command(Parser::CommandType t, double n1, double n2)
+    :   type{t},
+        num1{n1},
+        num2{n2}
+    {}
+
+
+size_t Parser::Command::printTo(Print& p) const {
+    size_t size = 0;
+    size += p.print("Command type: ");
+    size += p.print((int)type);
+    size += p.print(" (");
+    size += p.print(num1);
+    size += p.print(", ");
+    size += p.print(num2);
+    size += p.print(")");
+    return size;
+}
+
+Parser::CommandType Parser::processType(String commandString) {
+    #define parseCase(str, ct) if (commandString.equals(str)) {return CommandType::ct;}
+
+    parseCase("getstep", GetStep);
+    parseCase("gostep", GoStep);
+    parseCase("step", ShiftStep);
+
+    parseCase("getinch", GetInch);
+    parseCase("goinch", GoInch);
+    parseCase("inch", ShiftInch);
+
+    parseCase("getpos", GetPosition);
+    parseCase("go", Go);
+    parseCase("shift", Shift);
+
+    parseCase("fix", SoftZero);
+    parseCase("origin", HardZero);
+    #undef parseCase
+
+    // Shouldn't get here if we had a valid command.
+    return CommandType::Invalid;
+}
+
+static Parser::Command Parser::parse(String string) {
+    String intString1 = "";
+    String intString2 = "";
+
+    Serial.println("Entered: " + string);
+
+    int split1 = string.indexOf(" ");
+    int split2 = string.indexOf(" ", split1 + 1);
+
+    String commandString = string.substring(0, split1);
+
+    if (split1 > 0) {
+        if (split2 > 0) {
+            intString1 = string.substring(split1 + 1, split2);
+            intString2 = string.substring(split2 + 1);
+        } else {
+            intString1 = string.substring(split1 + 1);
+        }
+    }
+
+    Parser::CommandType type = Parser::processType(commandString);
+    double number1 = intString1.toDouble();
+    double number2 = intString2.toDouble();
+
+    return Parser::Command(type, number1, number2);
+}
--- eclipse/Parser.h
+++ eclipse/Parser.h
@@ -4,7 +4,7 @@
 #include <Arduino.h>
 
 class Parser {
-    public:
+public:
     enum class CommandType {
             Invalid,
             Test,  // test [testNumber] (executes a test in Test) [UNUSED]
@@ -24,63 +24,17 @@ class Parser {
             HardZero, // origin [str1] [str2] (declare current position as (0, 0) with str lengths in inches)
             SoftZero, // fix [str1] [str2] (adjust str lengths and position to match; keeps old origin)
     };
+
     class Command : public Printable {
-        /**
-         * @brief 
-         * e.g. goto 1 3
-         * e.g. step 0 20 (move left 20 steps)
-         * e.g. step 1 -30 (move right -30 steps)
-         */
-        public:
+    public:
+        Command(CommandType t, double n1, double n2);
+
+        size_t printTo(Print& p) const;
 
         CommandType type;
         double num1;
         double num2;
-
-        Command(CommandType t, double n1, double n2) {
-            type = t;
-            num1 = n1;
-            num2 = n2;
-        }
-
-        size_t printTo(Print& p) const {
-            size_t size = 0;
-            size += p.print("Command type: ");
-            size += p.print((int)type);
-            size += p.print(" (");
-            size += p.print(num1);
-            size += p.print(", ");
-            size += p.print(num2);
-            size += p.print(")");
-            return size;
-        }
     };
-
-    private:
-    CommandType processType(String commandString) {
-        #define parseCase(str, ct) if (commandString.equals(str)) {return CommandType::ct;}
-
-        parseCase("getstep", GetStep);
-        parseCase("gostep", GoStep);
-        parseCase("step", ShiftStep);
-
-        parseCase("getinch", GetInch);
-        parseCase("goinch", GoInch);
-        parseCase("inch", ShiftInch);
-
-        parseCase("getpos", GetPosition);
-        parseCase("go", Go);
-        parseCase("shift", Shift);
-
-        parseCase("fix", SoftZero);
-        parseCase("origin", HardZero);
-
-        // Shouldn't get here if we had a valid command.
-        return CommandType::Invalid;
-    }
-
-    public:
-    
 
     /**
      * @brief 
@@ -90,32 +44,14 @@ class Parser {
      *      - An enum Parser::Command telling us what's needed
      *      - Either 0 or the relevant number. 
      */
-    Command parse(String string) {
-        String intString1 = "";
-        String intString2 = "";
+    static Command parse(String string);
 
-        Serial.println("Entered: " + string);
+    static const Command empty;
 
-        int split1 = string.indexOf(" ");
-        int split2 = string.indexOf(" ", split1 + 1);
-
-        String commandString = string.substring(0, split1);
-
-        if (split1 > 0) {
-            if (split2 > 0) {
-                intString1 = string.substring(split1 + 1, split2);
-                intString2 = string.substring(split2 + 1);
-            } else {
-                intString1 = string.substring(split1 + 1);
-            }
-        }
-
-        CommandType type = processType(commandString);
-        double number1 = intString1.toDouble();
-        double number2 = intString2.toDouble();
-
-        return Command(type, number1, number2);
-    }
+private:
+    static CommandType processType(String commandString);
 };
+
+// const Parser::Command empty = Parser::Command(Parser::CommandType::Invalid, 0, 0);
 
 #endif
