796803c35b9fa56543fbf39ad9c3fa199e8f24d7
mchanphilly <56936452+mchanphilly@users.noreply.github.com>
2023-02-05 19:55:22 -0500

    Pull in updated Time from the example library

--- eclipse/Time.h
+++ eclipse/Time.h
@@ -4,23 +4,56 @@
 #include <Arduino.h>
 #include <TimeLib.h>
 
+/**
+ * @brief Might not be worth including in a class, but
+ * it's basically just to parse timeStrings and to print.
+ * 
+ * Mostly a wrapper class.
+ * 
+ */
 class Time : public Printable {
 public:
-    Time(unsigned long unix);
+    Time(time_t unix);
 
     Time(int year, int month, int day, int hour, int minute, int utcOffset=0);
     
     Time(const String timeString);
-    // Just learned about const functions. Huh.
-    // Prints to the Serial a human readable version of the time.
-    size_t printTo(Print& p) const;
-    
-    // void increment(int hour, int minute);
 
-    const time_t unixTime;
+    size_t printTo(Print& p) const;
+
+    // void increment(int hour, int minute);
+    static void setTime(Time time);
+    static void setTime(const String timeString);
+
+    // Accelerate the rate at which new time passes by a multiplier
+    static void setSpeed(double multiplier);
+
+    static void update();
+
+    static Time getNow();
+    static time_t getRawNow();
+
+    static Time fromMinutes(double minutes);
+
+    bool operator==(const Time& other) const;
+    bool operator<(const Time& other) const;
+    
+    Time operator+(const Time& other) const;
+
+    // todo check if this can be private
+    static double rate;
+
+    time_t unixTime;
 private:
-    const time_t listToUnix(int year, int month, int day, int hour, int minute, int utcOffset=0);
-    const time_t stringToUnix(String timeString);
+    time_t listToUnix(int year, int month, int day, int hour, int minute, int utcOffset=0);
+    time_t stringToUnix(String timeString);
+
+    inline static time_t seconds() {
+        return millis() / 1000;
+    }
+
+    static time_t now;
+    static time_t lastPolled;
 };
 
 #endif
